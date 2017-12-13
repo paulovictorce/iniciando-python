f64a449cc1f8a5fd07e9d5aa78b741789dd324c0
paulovictor <paulovictorce@hotmail.com>
2017-12-13 17:58:30 -0300

    Trabalhando com Functions

---
+++ basic_resources/functions.py
@@ -0,0 +1,11 @@
+def call_numbers():
+    for number in range(1,20):
+        print(number)
+
+def call_numbers_with_limit(limit):
+    for number in range(limit):
+        print(number)
+
+
+#call_numbers()
+call_numbers_with_limit(30)
