d74f063f8d1a46cacbedf6269017b555716faea5
paulovictor <paulovictorce@hotmail.com>
2017-12-19 17:54:44 -0300

    Trabalhando com Dicionários

---
+++ basic_resources/dict.py
@@ -0,0 +1,13 @@
+cars = {
+    'Idea':"Cinza",
+    'Gol': 'Prata',
+    '320Bmw': 'Vermelha'
+
+}
+
+print(cars)
+print(cars.keys())
+print(cars.values())
+
+if 'Gol' in cars:
+    print(cars['Gol'])
